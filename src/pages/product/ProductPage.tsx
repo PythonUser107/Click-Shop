28b480081297acf723d269223e791eb54ff5600a
Abdullox Xalimjonov <supercellgamesuser17@gmail.com>
2026-09-12 12:28:41 +0500

    first-commit

--- src/pages/product/ProductPage.tsx
+++ src/pages/product/ProductPage.tsx
@@ -1,10 +1,27 @@
-import {ProductCard} from "@/entities/product/ui/ProductCard" 
+import { useState, useEffect } from "react"
+import { ProductCard } from "@/entities/product/ui/ProductCard" 
 import { useGetProductsQuery } from "@/api/api" 
+import Loader from "@/shared/animations/Loader"
 
 function ProductPage() {
-  const { data: products, isLoading } = useGetProductsQuery()
+  const { data: products, isLoading: isApiLoading } = useGetProductsQuery()
+  const [isFakeLoading, setIsFakeLoading] = useState(true)
 
-  if (isLoading) return <div>Yuklanmoqda...</div>
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsFakeLoading(false)
+    }, 2000)
+
+    return () => clearTimeout(timer)
+  }, [])
+  
+  if (isApiLoading || isFakeLoading) {
+    return (
+      <div className="flex justify-center items-center min-h-[calc(100vh-77px)]">
+        <Loader size={500} />
+      </div>
+    )
+  }
 
   return (
     <div className="flex flex-wrap gap-4 p-5 justify-center">
